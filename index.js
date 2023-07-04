a0f073469bf3e68290e84c6d5e61dffa39ce2502
RobinMarche <robin.marche@gmail.com>
2023-07-04 22:25:18 +0200

    vercel

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/app/views');
 
-app.use(express.static('Ressources'))
+app.use(express.static(__dirname + '/Ressources'))
 
 const router = require('./app/router')
 
