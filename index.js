f0350174c060ffe6938187e206c51defa684fd07
RobinMarche <robin.marche@gmail.com>
2023-07-04 22:19:04 +0200

    vercel

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express()
 
 app.set('view engine', 'ejs');
-app.set('views', '/app/views');
+app.set('views', __dirname + '/app/views');
 
 app.use(express.static('Ressources'))
 
